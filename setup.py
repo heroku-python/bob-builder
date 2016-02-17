c96209c8cf7684d4961c16198c98809b660d24f1
Kenneth Reitz <me@kennethreitz.org>
2016-02-17 00:17:19 -0500

    v0.0.9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ deps = [
 
 setup(
     name='bob-builder',
-    version='0.0.8',
+    version='0.0.9',
     install_requires=deps,
     description='Binary Build Toolkit.',
     # long_description='Meh.',/
