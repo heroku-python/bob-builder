506a972125ce2dc52f761cac242b4057abb29184
David Zuelke <dzuelke@gmail.com>
2016-12-22 17:15:02 +0100

    v0.0.12

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ deps = [
 
 setup(
     name='bob-builder',
-    version='0.0.11',
+    version='0.0.12',
     install_requires=deps,
     description='Binary Build Toolkit.',
     # long_description='Meh.',/
