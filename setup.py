e30c2e53efdec0503bd183f6c4ab169a0ecf28e6
Ed Morley <edmorley@users.noreply.github.com>
2017-05-30 23:38:16 +0100

    v0.0.13

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ deps = [
 
 setup(
     name='bob-builder',
-    version='0.0.12',
+    version='0.0.13',
     install_requires=deps,
     description='Binary Build Toolkit.',
     # long_description='Meh.',/
