b3c45333349a3bf7439bb442c262305802de922b
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2023-03-01 08:37:20 -0600

    Adding tarfile member sanitization to extractall() (#52)

--- bob/utils.py
+++ bob/utils.py
@@ -52,7 +52,26 @@ def archive_tree(dir, archive):
 def extract_tree(archive, dir):
     """Extract tar.gz archive to a given directory."""
     with tarfile.open(archive, 'r:gz') as tar:
-        tar.extractall(dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, dir)
 
 # get a key, or the highest matching (as in software version) key if it contains wildcards
 # e.g. get_with_wildcard("foobar/dep-1.2.3") fetches that version
